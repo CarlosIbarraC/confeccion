13b78a99bf3a3691d707b950a3ce141577eb2a87
CarlosIbarraC <58642910+CarlosIbarraC@users.noreply.github.com>
2021-09-19 19:43:28 -0500

    dom

--- main/html/menu.php
+++ main/html/menu.php
@@ -488,63 +488,7 @@
   </div>
   <!-- / -->
    <!-- content -->
-   <div id="content" class="app-content box-shadow-z0" role="main">
-    <div class="app-header white box-shadow">
-        <div class="navbar navbar-toggleable-sm flex-row align-items-center">
-            <!-- Open side - Naviation on mobile -->
-            <a data-toggle="modal" data-target="#aside" class="hidden-lg-up mr-3">
-              <i class="material-icons">&#xe5d2;</i>
-            </a>
-            <!-- / -->
-        
-            <!-- Page title - Bind to $state's title -->
-            <div class="mb-0 h5 no-wrap" ng-bind="$state.current.data.title" id="pageTitle"></div>
-        
-            <!-- navbar collapse -->
-            <div class="collapse navbar-collapse" id="collapse">
-              <!-- link and dropdown -->
-              <ul class="nav navbar-nav mr-auto">
-                <li class="nav-item dropdown">
-                  <a class="nav-link" href data-toggle="dropdown">
-                    <i class="fa fa-fw fa-plus text-muted"></i>
-                    <span>New</span>
-                  </a>
-                  <div ui-include="'../views/blocks/dropdown.new.html'"></div>
-                </li>
-              </ul>
-        
-              <div ui-include="'../views/blocks/navbar.form.html'"></div>
-              <!-- / -->
-            </div>
-            <!-- / navbar collapse -->
-        
-            <!-- navbar right -->
-            <ul class="nav navbar-nav ml-auto flex-row">
-              <li class="nav-item dropdown pos-stc-xs">
-                <a class="nav-link mr-2" href data-toggle="dropdown">
-                  <i class="material-icons">&#xe7f5;</i>
-                  <span class="label label-sm up warn">3</span>
-                </a>
-                <div ui-include="'../views/blocks/dropdown.notification.html'"></div>
-              </li>
-              <li class="nav-item dropdown">
-                <a class="nav-link p-2 clear" href="#" data-toggle="dropdown">
-                  <span class="avatar w-32">
-                    <img src="../assets/images/foto-web.jpg" alt="...">
-                   <!--  <i class="off b-white bottom"></i> -->
-                  </span>
-                </a>
-                <div ui-include="'../views/blocks/dropdown.user.html'"></div>
-              </li>
-              <li class="nav-item hidden-md-up">
-                <a class="nav-link pl-2" data-toggle="collapse" data-target="#collapse">
-                  <i class="material-icons">&#xe5d4;</i>
-                </a>
-              </li>
-            </ul>
-            <!-- / navbar right -->
-        </div>
-    </div>
+   <div id="content" class="app-content box-shadow-z0" role="main">    
     <div class="app-footer">
       <div class="p-2 text-xs">
         <div class="pull-right text-muted py-1">
